be49fa03e33320f3e326ad13dd9e1bf002160fae
Aleksander <alekskr89@gmail.com>
2022-12-07 01:14:25 +0100

    Update calculate_geodesic_earth_orbit.cpp

--- examples/calculate_geodesic_earth_orbit.cpp
+++ examples/calculate_geodesic_earth_orbit.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-Matrix4real schwarzchild_cartesian(const Vector4real& x) {
+Matrix4real schwarzschild_cartesian(const Vector4real& x) {
 	double mass = 5.972e24;
 	Vector3d x0 = { 0.0,0.0,0.0 };
 	return AutoGeodesics::Metrics::schwarzchild_cartesian(x, mass, x0);
@@ -11,7 +11,7 @@ Matrix4real schwarzchild_cartesian(const Vector4real& x) {
 
 int main() {
 	AutoGeodesics ag = AutoGeodesics();
-	ag.setMetFn(&schwarzchild_cartesian);
+	ag.setMetFn(&schwarzschild_cartesian);
 	Vector4d x = { 0.0,6371000.0,0.0,0.0 };
 
 	Vector4d velocity = ag.setup_fourvelocity(x, Vector3d({ 0.0,sqrt(9.81997 * 6371000.0),0.0 })); //Turn velocity into 4-velocity.
