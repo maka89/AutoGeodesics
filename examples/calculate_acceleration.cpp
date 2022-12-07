e44844e70ba243b64c550cdfab59f0906542e6c4
Aleksander <alekskr89@gmail.com>
2022-12-07 01:14:03 +0100

    Update calculate_acceleration.cpp

--- examples/calculate_acceleration.cpp
+++ examples/calculate_acceleration.cpp
@@ -1,9 +1,9 @@
 #include <autogeodesics.h>
 #include <iostream>
-Matrix4real schwarzchild_cartesian(const Vector4real& x) {
+Matrix4real schwarzschild_cartesian(const Vector4real& x) {
 	double mass = 5.972e24;
 	Vector3d x0 = { 0.0,0.0,0.0 };
-	return AutoGeodesics::Metrics::schwarzchild_cartesian(x, mass, x0);
+	return AutoGeodesics::Metrics::schwarzschild_cartesian(x, mass, x0);
 }
 
 int main() {
